881921f6a52c39a2ff92fe874abe53dbc7ebf735
Nicola Kaleta <nic00la.kaleta@gmail.com>
2024-02-14 08:49:50 +0100

    background img + shadow circle

---
+++ Views/Onboarding.swift
@@ -0,0 +1,44 @@
+//
+//  Onboarding.swift
+//  Nike-App
+//
+//  Created by Nicola Kaleta on 14/02/2024.
+//
+
+import SwiftUI
+
+struct Onboarding: View {
+    
+    @State private var isActive = false
+    
+    var body: some View {
+        if isActive {
+            // We move to Login Screen after that
+        } else {
+            // Design of This View
+            ZStack(alignment: .top) {
+                VStack {
+                    Spacer()
+                    // Add a red circle in bottom that show animation
+                    Circle()
+                        .fill(
+                            RadialGradient(colors:[ .red, .clear, .clear, .clear], center: .center, startRadius: 0, endRadius: UIScreen.main.bounds.width)
+                        )
+                }
+            }
+            .padding()
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+            .background(
+                Image("onboarding_back")
+                    .resizable()
+                    .scaledToFill()
+                    .ignoresSafeArea()
+            )
+            .preferredColorScheme(.dark)
+        }
+    }
+}
+
+#Preview {
+    Onboarding()
+}
