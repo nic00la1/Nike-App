b6e5a8854aa632d4c0723e044a39a6f60cf91f7f
Nicola Kaleta <nic00la.kaleta@gmail.com>
2024-02-17 18:52:52 +0100

    navigation ui

--- Views/Product.swift
+++ Views/Product.swift
@@ -21,16 +21,43 @@ struct Product: View {
                                 .frame(height: 400)
                                 .clipShape(RoundedRectangle(cornerRadius: 50))
                                 .padding(10)
+                            
+                            // Navigation
+                            HStack {
+                                Image(systemName: "arrow.left")
+                                
+                                Spacer()
+                                
+                                Image(systemName: "heart")
+                            }
+                            .padding(.top, safeArea().top)
+                            .padding(.horizontal, 30)
+                            .imageScale(.large)
                         }
                     }
                 }
                 
                 // Bottom Bar
             }
+            .ignoresSafeArea()
         }
     }
 }
 
 #Preview {
     Product()
+}
+
+// For padding for safe area we need the size because different devices have different size of safe area
+extension View {
+    func safeArea() -> UIEdgeInsets {
+        guard let screen = UIApplication.shared.connectedScenes.first as? UIWindowScene else {
+            return .zero
+        }
+        
+        guard let safeAera = screen.windows.first?.safeAreaInsets else {
+            return .zero
+        }
+        return safeAera
+    }
 }
