91aeef6eda7862cf4edfb3e7aec0bfc00279e0cd
Nicola Kaleta <nic00la.kaleta@gmail.com>
2024-02-17 13:39:44 +0100

    tabItemStyle extension

--- ContentView.swift
+++ ContentView.swift
@@ -9,16 +9,47 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        TabView {
+            // Bottom Bar
+            Home()
+                .tabItem {
+                    Image(systemName: "house")
+                    Text("Home")
+                }
+                .tabItemStyle
+                
+            Text("Search View")
+                .tabItem {
+                    Image(systemName: "magnifyingglass")
+                    Text("Explore")
+                }
+                .tabItemStyle
+            
+            Text("Cart View")
+                .tabItem {
+                    Image(systemName: "bag")
+                    Text("Cart")
+                }
+                .tabItemStyle
+            
+            Text("Profile View")
+                .tabItem {
+                    Image(systemName: "person")
+                    Text("Profile")
+                }
+                .tabItemStyle
         }
-        .padding()
     }
 }
 
 #Preview {
     ContentView()
+}
+
+extension View {
+    var tabItemStyle: some View {
+        self
+            .toolbarBackground(.visible, for: .tabBar)
+            .toolbarBackground(.ultraThinMaterial, for: .tabBar)
+    }
 }
